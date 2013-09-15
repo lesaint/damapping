3873df6997dbf6cfbd294bcdce45f152f913c1c5
Sebastien Lesaint <lesaint@fullsix.com>
2013-09-15 14:30:58 +0200

    renomage IntegerToString vers nom technique

--- demo/src/main/java/com/ekino/lesaint/dozerannihilation/demo/ConstructorInstancedGuavaFunctionMappar.java
+++ demo/src/main/java/com/ekino/lesaint/dozerannihilation/demo/ConstructorInstancedGuavaFunctionMappar.java
@@ -7,7 +7,7 @@ import com.google.common.base.Function;
 import javax.annotation.Nullable;
 
 @Mapper(InstantiationType.CONSTRUCTOR)
-public class IntegerToString implements Function<Integer, String> {
+public class ConstructorInstancedGuavaFunctionMappar implements Function<Integer, String> {
 
     @Override
     public String apply(@Nullable Integer input) {
