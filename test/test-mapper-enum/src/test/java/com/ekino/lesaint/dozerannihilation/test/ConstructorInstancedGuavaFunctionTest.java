3c0d4cfc422ad65f0658f3c5bfe9624a117a7a0c
Sébastien Lesaint <lesaint@ekino.com>
2013-09-20 18:58:44 +0200

    ajout de testNG et d'un test U vide

---
+++ test/test-mapper-enum/src/test/java/com/ekino/lesaint/dozerannihilation/test/ConstructorInstancedGuavaFunctionTest.java
@@ -0,0 +1,15 @@
+package com.ekino.lesaint.dozerannihilation.test;
+
+import org.testng.annotations.Test;
+
+/**
+ * ConstructorInstancedGuavaFunctionTest -
+ *
+ * @author lesaint
+ */
+public class ConstructorInstancedGuavaFunctionTest {
+    @Test
+    public void testApply() throws Exception {
+        Class<ConstructorInstancedGuavaFunction> constructorInstancedGuavaFunctionClass = ConstructorInstancedGuavaFunction.class;
+    }
+}
