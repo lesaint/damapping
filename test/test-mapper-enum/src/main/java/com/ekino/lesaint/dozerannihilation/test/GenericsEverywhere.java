3f1c752d86a0df8e8e3cb4d6807ffecb2001c6b2
Sébastien Lesaint <lesaint@ekino.com>
2013-09-29 23:28:12 +0200

    ajout d'un test sur les generics
    le test échoue

---
+++ test/test-mapper-enum/src/main/java/com/ekino/lesaint/dozerannihilation/test/GenericsEverywhere.java
@@ -0,0 +1,23 @@
+package com.ekino.lesaint.dozerannihilation.test;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import javax.annotation.Nullable;
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+
+import com.ekino.lesaint.dozerannihilation.annotation.InstantiationType;
+import com.ekino.lesaint.dozerannihilation.annotation.Mapper;
+
+@Mapper(InstantiationType.SINGLETON_ENUM)
+public enum GenericsEverywhere implements Function<Optional<Integer[]>, Collection<Predicate<Map<Long, File>>>> {
+    INSTANCE;
+
+    @Override
+    public Collection<Predicate<Map<Long, File>>> apply(@Nullable Optional<Integer[]> input) {
+        return Collections.emptyList();
+    }
+}
---
+++ test/test-mapper-enum/src/test/java/com/ekino/lesaint/dozerannihilation/test/GenericsEverywhereTest.java
@@ -0,0 +1,14 @@
+package com.ekino.lesaint.dozerannihilation.test;
+
+/**
+ * GenericsEverywhereTest -
+ *
+ * @author lesaint
+ */
+public class GenericsEverywhereTest extends AbstractMapperTest {
+
+    public GenericsEverywhereTest() {
+        super(GenericsEverywhere.class);
+    }
+
+}
