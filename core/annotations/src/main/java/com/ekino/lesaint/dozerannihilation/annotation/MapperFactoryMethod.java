27769f1850995e85826e004fab68cdf5c83569c7
Sebastien Lesaint <lesaint@ekino.com>
2013-10-12 23:44:18 +0200

    MapperFactoryMethod can also be on constructor

--- core/annotations/src/main/java/com/ekino/lesaint/dozerannihilation/annotation/MapperFactoryMethod.java
+++ core/annotations/src/main/java/com/ekino/lesaint/dozerannihilation/annotation/MapperFactoryMethod.java
@@ -5,7 +5,7 @@ package com.ekino.lesaint.dozerannihilation.annotation;
  *
  * @author Sébastien Lesaint
  */
-@java.lang.annotation.Target({java.lang.annotation.ElementType.METHOD})
+@java.lang.annotation.Target({java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.CONSTRUCTOR})
 @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.SOURCE)
 @java.lang.annotation.Documented
 public @interface MapperFactoryMethod {
