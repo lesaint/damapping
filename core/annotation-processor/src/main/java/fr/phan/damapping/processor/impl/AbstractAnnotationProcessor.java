50a55b68936411bf827be01cac9182cdb19daa51
Sébastien Lesaint <lesaint@ekino.com>
2013-12-15 23:48:34 +0100

    ajout capture toute exception pour donner context
    on capture désormais toute Exception levée lors du processing des annotations et on rethrow une RuntimeException qui précise la classe et l'annotation pour lesquels l'erreur s'est produite

--- core/annotation-processor/src/main/java/fr/phan/damapping/processor/impl/AbstractAnnotationProcessor.java
+++ core/annotation-processor/src/main/java/fr/phan/damapping/processor/impl/AbstractAnnotationProcessor.java
@@ -18,11 +18,16 @@ package fr.phan.damapping.processor.impl;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.util.Set;
+import javax.annotation.Nullable;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
 
 /**
  * AbstractAnnotationProcessor -
@@ -39,21 +44,78 @@ public abstract class AbstractAnnotationProcessor<T extends Annotation> implemen
     }
 
     @Override
-    public void processAll(TypeElement annotation, RoundEnvironment roundEnv) {
+    public void processAll(final TypeElement annotation, RoundEnvironment roundEnv) {
         Set<? extends Element> elementsAnnotatedWith = roundEnv.getElementsAnnotatedWith(annotationType);
-//        System.out.println("Elements retrieved " + elementsAnnotatedWith);
         for (Element element : elementsAnnotatedWith) {
             try {
                 process(element, roundEnv);
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
+                // In Maven, this message is actually not visible, I don't know yet about Oracle javac or Open JDK
                 processingEnv.getMessager().printMessage(
                         Diagnostic.Kind.ERROR,
-                        "Processing of elements annotated with " + annotationType.getCanonicalName() + " failed for " + element.getSimpleName(),
-                        element
+                        buildErrorMessage(e, annotation),
+                        element,
+                        getAnnotationMirror(annotation, element)
+                );
+                throw new RuntimeException(
+                        String.format(
+                                "Exception occured while processing annotation @%s on %s",
+                                annotation.getSimpleName(),
+                                element.asType().toString()
+                        ),
+                        e
                 );
             }
         }
+    }
+
+    /**
+     * Produit un message d'erreur indiquant que le traitement de l'annoation {@code annotation} a échoué avec
+     * l'exception indiquée et précise la première ligne de la stacktrace si l'information est disponible.
+     *
+     * @param e l'{@link Exception} capturée
+     * @param annotation un {@link TypeElement} représentation la classe d'une annotation
+     *
+     * @return une {@link String}
+     */
+    private static String buildErrorMessage(Exception e, TypeElement annotation) {
+        StackTraceElement[] stackTrace = e.getStackTrace();
+        StringBuilder builder = new StringBuilder()
+                .append("Processing of annotation ")
+                .append(annotation.getSimpleName())
+                .append(" : ")
+                .append(e);
+        if (stackTrace.length > 0) {
+            builder.append(" at ").append(stackTrace[0]);
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Récupère l'AnnotationMirror sur l'Element spécifié qui correspond à l'annotation traitée par
+     * l'AnnotationProcessor dont le TypeElement est spécifié.
+     * </p>
+     * Cela permet de connaitre la ligne dans les sources où se trouver l'annotation traitée et de contextualiser
+     * encore plus finement le message d'erreur à la compilation.
+     *
+     * @param annotation un {@link TypeElement} représentation la classe d'une annotation
+     * @param element un {@link Element} sur lequel est posé l'annotation
+     *
+     * @return un {@link AnnotationMirror} ou {@code null}
+     */
+    @Nullable
+    private AnnotationMirror getAnnotationMirror(final TypeElement annotation, final Element element) {
+        Optional<? extends AnnotationMirror> annotationMirror = FluentIterable.from(element.getAnnotationMirrors())
+                .filter(new Predicate<AnnotationMirror>() {
+                    @Override
+                    public boolean apply(@Nullable AnnotationMirror o) {
+                        return processingEnv.getTypeUtils().isSameType(o.getAnnotationType(), annotation.asType());
+                    }
+                }).first();
+        if (annotationMirror.isPresent()) {
+            return annotationMirror.get();
+        }
+        return null;
     }
 
     protected abstract void process(Element element, RoundEnvironment roundEnv) throws IOException;
