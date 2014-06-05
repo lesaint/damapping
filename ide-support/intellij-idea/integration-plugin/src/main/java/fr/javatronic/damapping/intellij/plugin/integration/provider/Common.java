744fc6b4182c8f5ca0611139da2fd6ea40d644b3
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2014-06-05 18:39:18 +0200

    statically caching generated PsiClass is actually not necessary
    still, generating the same PsiClass multiple times if not efficient, need to be cached/indexed

--- ide-support/intellij-idea/integration-plugin/src/main/java/fr/javatronic/damapping/intellij/plugin/integration/provider/Common.java
+++ ide-support/intellij-idea/integration-plugin/src/main/java/fr/javatronic/damapping/intellij/plugin/integration/provider/Common.java
@@ -25,33 +25,24 @@ public class Common {
       "\n" +
       "}\n";
 
-  private static PsiClass teacherToPeopleMapper;
-  private static PsiClass studentToPeopleMapper;
-
   public static PsiClass generateClass(GlobalSearchScope scope, String name) {
-        if (name.contains("TeacherToPeople")) {
-          if (teacherToPeopleMapper == null) {
-            PsiJavaFile psiJavaFile = (PsiJavaFile) PsiFileFactory.getInstance(scope.getProject())
-                                                                  .createFileFromText("TeacherToPeopleMapper.java",
-                                                                      JavaFileType.INSTANCE, TeacherToPeopleMapper
-                                                                  );
+    if (name.contains("TeacherToPeople")) {
+      PsiJavaFile psiJavaFile = (PsiJavaFile) PsiFileFactory.getInstance(scope.getProject())
+                                                            .createFileFromText("TeacherToPeopleMapper.java",
+                                                                JavaFileType.INSTANCE, TeacherToPeopleMapper
+                                                            );
 
-            teacherToPeopleMapper = psiJavaFile.getClasses()[0];
-          }
-          return teacherToPeopleMapper;
-        }
+      return psiJavaFile.getClasses()[0];
+    }
 
-        if (name.contains("StudentToPeople")) {
-          if (studentToPeopleMapper == null) {
-            PsiJavaFile psiJavaFile = (PsiJavaFile) PsiFileFactory.getInstance(scope.getProject())
-                                                                  .createFileFromText("StudentToPeopleMapper.java",
-                                                                      JavaFileType.INSTANCE, StudentToPeopleMapper
-                                                                  );
-            studentToPeopleMapper = psiJavaFile.getClasses()[0];
-          }
-          return studentToPeopleMapper;
-        }
+    if (name.contains("StudentToPeople")) {
+      PsiJavaFile psiJavaFile = (PsiJavaFile) PsiFileFactory.getInstance(scope.getProject())
+                                                            .createFileFromText("StudentToPeopleMapper.java",
+                                                                JavaFileType.INSTANCE, StudentToPeopleMapper
+                                                            );
+      return psiJavaFile.getClasses()[0];
+    }
 
-        return null;
-      }
+    return null;
+  }
 }
