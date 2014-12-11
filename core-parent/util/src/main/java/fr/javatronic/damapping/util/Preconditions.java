beb5b73264049f9943f53a6337516f19d3347d72
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2014-12-11 11:52:31 +0100

    [#52] add Preconditions.checkArgumen(boolean, String)
    add method checkArgument which supports specifying the IllegalArgumentException message
    add javadoc on methods

--- core-parent/util/src/main/java/fr/javatronic/damapping/util/Preconditions.java
+++ core-parent/util/src/main/java/fr/javatronic/damapping/util/Preconditions.java
@@ -15,30 +15,82 @@
  */
 package fr.javatronic.damapping.util;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
  * Preconditions - Partial clone of Guava's Preconditions class
  *
  * @author Sébastien Lesaint
  */
 public final class Preconditions {
+
+  private static final String NPE_DEFAULT_MSG = "object can not be null";
+  private static final String IAE_DEFAULT_MSG = "Argument is not valid";
+
   private Preconditions() {
     // prevents instantiation
   }
 
-  public static <T> T checkNotNull(T obj) {
-    return checkNotNull(obj, "object can not be null");
+  /**
+   * Throws a NullPointerException with a generic message if the specified object is {@code null}, otherwise
+   * returns it.
+   *
+   * @param obj an object of any type or {@code null}
+   * @param <T> any type
+   *
+   * @return the argument
+   */
+  @Nonnull
+  public static <T> T checkNotNull(@Nullable T obj) {
+    return checkNotNull(obj, NPE_DEFAULT_MSG);
   }
 
-  public static <T> void checkArgument(boolean test) {
-    if (!test) {
-      throw new IllegalArgumentException("Argument is not valid");
-    }
-  }
-
-  public static <T> T checkNotNull(T obj, String message) {
+  /**
+   * Throws a NullPointerException with the specified message if the specified object is {@code null}, otherwise
+   * returns it.
+   * <p>
+   * A default message will be used if the specified message is {@code null} or empty.
+   * </p>
+   *
+   * @param obj     an object of any type or {@code null}
+   * @param message a {@link String} or {@code null}
+   * @param <T>     any type
+   *
+   * @return the argument
+   */
+  @Nonnull
+  public static <T> T checkNotNull(@Nullable T obj, @Nullable String message) {
     if (obj == null) {
-      throw new NullPointerException(message);
+      throw new NullPointerException(message == null || message.isEmpty() ? NPE_DEFAULT_MSG : message);
     }
     return obj;
+  }
+
+  /**
+   * Throws a {@link IllegalArgumentException} with a generic message if the specified boolean value is false.
+   *
+   * @param test a boolean value
+   */
+  public static void checkArgument(boolean test) {
+    if (!test) {
+      throw new IllegalArgumentException(IAE_DEFAULT_MSG);
+    }
+  }
+
+  /**
+   * Throws a {@link IllegalArgumentException} with the specified message if the specified boolean
+   * value is false.
+   * <p>
+   * A default message will be used if the specified message is {@code null} or empty.
+   * </p>
+   *
+   * @param test    a boolean value
+   * @param message a {@link String} or {@code null}
+   */
+  public static void checkArgument(boolean test, @Nullable String message) {
+    if (!test) {
+      throw new IllegalArgumentException(message == null || message.isEmpty() ? IAE_DEFAULT_MSG : message);
+    }
   }
 }
